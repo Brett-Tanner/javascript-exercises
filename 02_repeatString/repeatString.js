7b73d0c8eb6c974c47f9490f409b36d75627db06
Brett <bretttanner171@gmail.com>
2022-03-23 15:26:51 +0900

    Pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+    let stringResult = "";
+    
+    if (n < 0) return "ERROR";
 
+    for (i = 0; i < n; i++) {
+        stringResult = stringResult + string;        
+    }
+    return stringResult;
 };
 
 // Do not edit below this line
