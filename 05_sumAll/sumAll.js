970361c2bbf657c916f92fbbd891753c39c95448
Brett <bretttanner171@gmail.com>
2022-03-25 12:04:20 +0900

    Pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    // accumulator variable
+    let runningTotal = 0;
+    // checks if either parameter is negative or NaN, returns ERROR if so. Used typeof rather than isNaN() because isNaN maybe converts strings first??
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    }
+        else if (num1 < num2) {
+            for (i = num1; i <= num2; i++) {
+                runningTotal += i;
+            }
+        }
+        else {
+            for (i = num1; i >= num2; i--) {
+                runningTotal += i;
+            }
+        }
+    return runningTotal;
 };
 
 // Do not edit below this line
