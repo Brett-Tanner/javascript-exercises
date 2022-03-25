d7ff96d79bfaa0b09409b7fe04fa4cdd14108698
Brett <bretttanner171@gmail.com>
2022-03-25 12:24:03 +0900

    Pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahr) {
+  return Math.round((tempFahr - 32) * 5 / 9 * 10) / 10
 };
 
-const ctof = function() {
+// [°C] = ([°F] − 32) × 5⁄9
 
+const ctof = function(tempCels) {
+  return Math.round((tempCels * 9 / 5 + 32) * 10) /10
 };
+
+
+// [°F] = [°C] × 9⁄5 + 32
 
 // Do not edit below this line
 module.exports = {
