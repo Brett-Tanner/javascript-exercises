dd6bd1f59905a67142d186673122c5778ef92658
Brett <bretttanner171@gmail.com>
2022-03-25 11:24:37 +0900

    Pass tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedValues) {
+    return array.filter((element) => {
+        // Checks the current array element against each element of the removedValues array, returns false if they're equal
+        for (i = 0; i < removedValues.length; i++) {
+            if (element === removedValues[i]) {
+                return false;
+            } 
+        }
+        // Returns true if no equal values are found, filtering the element into the new array
+        return true;
+    });
 };
 
 // Do not edit below this line
