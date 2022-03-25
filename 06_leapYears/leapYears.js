59188923f08386674f504a36e9b65a56d1dee55c
Brett <bretttanner171@gmail.com>
2022-03-25 12:15:22 +0900

    Pass tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    }
+        else if (year % 400 === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
 };
+
+// divisible by 4 = true, not divisible by 100 = true
+// divisible by 400 = true
+// else false
 
 // Do not edit below this line
 module.exports = leapYears;
