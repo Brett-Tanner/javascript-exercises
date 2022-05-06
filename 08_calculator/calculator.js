894fe98e9f367bb269334626d1a25c741917edf1
Brett <bretttanner171@gmail.com>
2022-05-06 21:40:15 +0900

    Write code, needs test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	array.reduce(a, b => a + b);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  array.reduce(a, b => a * b);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let storage = num;
+	for (i = (num - 1); i > 0; i--) {
+    storage *= i;
+  };
+  return storage;
 };
 
 // Do not edit below this line
