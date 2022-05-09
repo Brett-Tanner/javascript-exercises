87ca81699fa6f1fe98b07dd00d2f45cc239715e3
Brett Tanner <bretttanner171@gmail.com>
2022-05-09 20:25:48 +0900

    Optimise check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,26 +2,14 @@ const palindromes = function (string) {
     // set the whole thing to lowercase
     string = string.toLowerCase();
     // convert to array
-    let array = Array.from(string);
+    const array = Array.from(string);
     // filter out punctuation/spaces 
-    let filteredArray = array.filter(letter => letter >= "a" && letter <= "z");
+    const filteredArray = array.filter(letter => letter >= "a" && letter <= "z");
     // check remaining letters to see if they're a palindrome 
-    try {
-    let loop = 0;
-    filteredArray.forEach(element => {
-        let compareIndex = filteredArray.length - 1 - loop;
-        if (element !== filteredArray[compareIndex]) {
-            loop = 0;
-            throw "BreakError";
-        }
-        loop++;
+    return filteredArray.every((element, index) => {
+        let compareIndex = filteredArray.length - 1 - index;
+        return filteredArray[compareIndex] === element;
     });
-    } catch (err) {
-        if (err === "BreakError") {
-            return false;
-        }
-    }
-    return true;
 };
 
 // Do not edit below this line
