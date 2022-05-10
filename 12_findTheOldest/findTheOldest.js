ab0afca7c0e670d282871313dc3beb5a163aa40e
Brett Tanner <bretttanner171@gmail.com>
2022-05-10 17:22:54 +0900

    Tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce(function(previousValue, currentValue) {
+        let previousAge;
+        let currentAge;
+        const currentYear = new Date().getFullYear();
+        if (previousValue.yearOfDeath === undefined) {
+            previousAge = currentYear - previousValue.yearOfBirth;
+        }
+        else {
+            previousAge = previousValue.yearOfDeath - previousValue.yearOfBirth;
+        };
+        if (currentValue.yearOfDeath === undefined) {
+            currentAge = currentYear - currentValue.yearOfBirth;
+        }
+        else {
+            currentAge = currentValue.yearOfDeath - currentValue.yearOfBirth;
+        };
+        if (currentAge > previousAge) {
+            return currentValue;
+        }
+        else {
+            return previousValue;
+        };
+    })
 };
 
 // Do not edit below this line
