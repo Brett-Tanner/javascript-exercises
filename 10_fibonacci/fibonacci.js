fa4cde9da084b4356312dc7527abfa110261a911
Brett <bretttanner171@gmail.com>
2022-05-10 16:14:35 +0900

    Draft code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) {
+        return "OOPS";
+    }
+    let twoBefore = 0;
+    let oneBefore = 0;
+    let result = 1;
+    for (i = 1; i < n; i++) {
+        result = twoBefore + oneBefore;
+        twoBefore = oneBefore;
+        oneBefore = result;
+    };
+    return result;
 };
 
 // Do not edit below this line
